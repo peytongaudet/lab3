a11a50f3824947655605e975bab3cb025a63677d
peytongaudet <pgaudet@ucsd.edu>
2022-10-13 15:12:51 -0700

    part 1

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,20 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReverseInPlace2()
+  {
+    int[] input1 = {1, 3, 2, 4} ;
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{4, 2, 3, 1}, input1);
+  }
+
+  @Test
+  public void testReversed2()
+  {
+    int[] input1 = {1, 3, 2, 4};
+    assertArrayEquals(new int[]{4, 2, 3, 1}, ArrayExamples.reversed(input1));
+  }
+
 }
